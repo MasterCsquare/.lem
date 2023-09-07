787e58bab1dfcda7c2b9872533c873ad1b6ec0b1
steiner <yasteiner@protonmail.com>
2023-09-07 18:21:55 +0800

    first commit

---
+++ init.lisp
@@ -0,0 +1,19 @@
+(in-package :lem-user)
+
+(load-theme "onedark")
+
+(asdf:load-system :lem-pareto)
+(add-hook lem-lisp-mode:*lisp-mode-hook* 'lem-paredit-mode:paredit-mode)
+(add-hook lem-lisp-mode:*lisp-mode-hook* 'lem-pareto-mode:pareto-mode)
+
+(define-key lem-lisp-mode:*lisp-mode-keymap* "F1" 'lem-lisp-mode:lisp-describe-symbol)
+
+(add-hook (variable-value 'before-save-hook :global) 'delete-trailing-whitespace)
+
+(lem-sdl2:change-font
+ (lem-sdl2/font:make-font-config
+  :size 18
+  :latin-normal-file "/usr/share/fonts/TTF/Inconsolata-Regular.ttf"
+  :latin-bold-file "/usr/share/fonts/TTF/Inconsolata-Bold.ttf"
+  :cjk-normal-file "/usr/share/fonts/wenquanyi/wqy-zenhei/wqy-zenhei.ttc"
+  :cjk-bold-file "/usr/share/fonts/wenquanyi/wqy-zenhei/wqy-zenhei.ttc"))
