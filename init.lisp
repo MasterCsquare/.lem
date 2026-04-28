ff4e7f1afd827217d2aa281fb82b7df556900945
steiner <yasteiner@protonmail.com>
2026-04-28 22:44:09 +0800

    remove SDL2 related code

--- init.lisp
+++ init.lisp
@@ -9,11 +9,3 @@
 (define-key lem-lisp-mode:*lisp-mode-keymap* "F1" 'lem-lisp-mode:lisp-describe-symbol)
 
 (add-hook (variable-value 'before-save-hook :global) 'delete-trailing-whitespace)
-
-;; (lem-sdl2/display:change-font (lem-sdl2/display:current-display)
-;;  (lem-sdl2/font:make-font-config
-;;   :size 18
-;;   :latin-normal-file "/usr/share/fonts/TTF/Inconsolata-Regular.ttf"
-;;   :latin-bold-file "/usr/share/fonts/TTF/Inconsolata-Bold.ttf"
-;;   :cjk-normal-file "/usr/share/fonts/wenquanyi/wqy-zenhei/wqy-zenhei.ttc"
-;;   :cjk-bold-file "/usr/share/fonts/wenquanyi/wqy-zenhei/wqy-zenhei.ttc"))
